1a70105f484db1557f6799eafd1ebf42ce72eb1a
Julien-Turcotte <6315237@cegepoutaouais.qc.ca>
2025-04-03 09:51:18 -0400

    afwafawsd

--- reader.py
+++ reader.py
@@ -1,18 +1,26 @@
+# codé par Julien
+
+
+
 import csv
 
 
-def reading(filename, itemname, nutriment_cherche):
+def reading(filename, nbr, v_nbr):
+
     """
+    Prend un numero de ligne et retoure la ligne dans un fichier csv
 
     :param filename: le nom du file
-    :param itemname:
+    :param nbr: le numero de ligne du fichier
+    :param v_nbr: le numero de l'element dans la ligne du fichier
     :return: la valeur de l'élément cherché
     """
     with open(filename, newline='', encoding="utf8") as csvfile:
         reader = csv.reader(csvfile)
-        for row in reader:
-            element = {}
-            print(row)
 
+        r_ligne = [ligne for idx, ligne in enumerate(reader)if idx == nbr] #ch3ka sur stackoverflow https://stackoverflow.com/questions/26464567/csv-read-specific-row
+        r_val = [valeur for idx, valeur in enumerate(r_ligne)if idx == v_nbr]
 
-reading("data.csv", "banane", "Énergie (kcal)")
+    return r_val
+
+print(reading("data.csv", 2, 5))
